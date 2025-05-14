1e413de7bde55280dd0c90ece9e35229144d6cdc
Duks <duksjune19962@gmail.com>
2025-05-14 13:16:41 +0530

    test1

--- examples/examples/src/main/java/com/apiexample/entity/A.java
+++ examples/examples/src/main/java/com/apiexample/entity/A.java
@@ -3,6 +3,6 @@ package com.apiexample.entity;
 public class A {
     public static void main(String[] args) {
         System.out.println("hello world! from A 983");
-        System.out.println("master branch but not from develop branch");
+        System.out.println("master branch");
     }
 }
