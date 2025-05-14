fa0f0c4e77e34b6e230bb9ff520ab82fc09b48af
Duks <duksjune19962@gmail.com>
2025-05-14 12:46:51 +0530

    adding class A 982

--- examples/examples/src/main/java/com/apiexample/entity/A.java
+++ examples/examples/src/main/java/com/apiexample/entity/A.java
@@ -2,6 +2,6 @@ package com.apiexample.entity;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("hello world! from A");
+        System.out.println("hello world! from A 982");
     }
 }
